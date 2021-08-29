043506cecfbfc0dd8d0395a7b001374b8bd085fe
Tushar Chouhan <86911541+Ske111eton@users.noreply.github.com>
2021-08-29 21:03:05 +0530

    Add files via upload

---
+++ Practice.java
@@ -0,0 +1,104 @@
+package practice;
+
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data = data;
+        next = null;
+    }
+}
+
+class MyLinkedList{
+    Node head;
+
+    public void add(int data){
+        Node node = new Node(data);
+        if(head == null){
+            head = node;
+        }
+        else{
+            Node temp = head;
+            while(temp.next != null){
+                temp = temp.next;
+            }
+            temp.next = node;
+            node.next = null;
+        }
+    }
+    public int remove() throws Exception{
+        Node temp = head;
+        if(head==null){
+            throw new Exception("Empty LinkedList");
+        }
+
+            if(temp.next == null){
+                Node toRemove = head;
+                head = null;
+                return toRemove.data;
+            }
+            while(temp.next.next != null){
+                temp = temp.next;
+            }
+            Node toRemove = temp.next;
+            temp.next = null;
+            return toRemove.data;
+    }
+
+    public int getLast() throws Exception{
+        if(head == null){
+            throw new Exception("Empty linked list");
+        }
+        Node temp = head;
+        while(temp.next!= null){
+            temp =temp.next;
+        }
+        return temp.data;
+    }
+    public boolean isEmpty(){
+        return head == null;
+    }
+
+    public void show(){
+        if(isEmpty()){
+            System.out.println("Empty");
+        }
+        Node temp = head;
+        while(temp.next != null){
+            System.out.print(temp.data + " ");
+            temp =temp.next;
+        }
+        System.out.println(temp.data);
+    }
+}
+
+class MyStack{
+    MyLinkedList stack = new MyLinkedList();
+
+    public void push(int data){
+        stack.add(data);
+    }
+    public int pop() throws Exception{
+        if(stack.isEmpty()){
+            throw new Exception("Empty stack");
+        }
+        return stack.remove();
+    }
+    public int peek() throws Exception{
+        if(stack.isEmpty()){
+            throw new Exception("Empty stack");
+        }
+        return stack.getLast();
+    }
+public void display(){
+        stack.show();
+}
+
+}
+
+public class Practice {
+    public static void main(String[] args) throws Exception{
+        MyStack s = new MyStack();
+
+    }
+}
